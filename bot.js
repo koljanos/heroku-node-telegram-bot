4027b14e4dd3589333bb696076ad3658d7179605
koljanos <bohrock@yandex.ru>
2017-09-07 21:45:54 +0300

    removed

--- bot.js
+++ bot.js
@@ -21,16 +21,3 @@ bot.onText(/^/, function (msg) {
 });
 
 module.exports = bot;
-
-var svr = http.createServer(function(req, resp) {
-   var body = "";
-  req.on('data', function (chunk) {
-    body += chunk;
-  });
-  req.on('end', function () {
-    console.log('body: ' + body);
-    var jsonObj = JSON.parse(body);
-  console.log(jsonObj.$key);
-  })
-    resp.end('Hello, World!');
-});
